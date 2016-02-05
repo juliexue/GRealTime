e994727c338889ba8a853a7d80336655e7d84b7f
Julie Xue <julie.xue11@gmail.com>
2016-02-04 20:07:52 -0500

    added 1 stop

--- app/src/main/java/com/grealtime/grealtime/MainActivity.java
+++ app/src/main/java/com/grealtime/grealtime/MainActivity.java
@@ -1,6 +1,7 @@
 package com.grealtime.grealtime;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
--- app/src/main/java/com/grealtime/grealtime/StopActivity.java
+++ app/src/main/java/com/grealtime/grealtime/StopActivity.java
@@ -1,5 +1,7 @@
 package com.grealtime.grealtime;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,8 @@ public class StopActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stop);
+        Intent smsIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:57555"));
+        smsIntent.putExtra("sms_body", "2674");
+        startActivity(smsIntent);
     }
 }
